550da22710000235653e605bb5870ad255dfd472
pattisoa <pattisoa@OA-S141-23>
2026-01-13 18:53:58 -0500

    cleaning up scripts

--- Prog4/Assets/Scripts/MoveAndRotateTowards.cs
+++ Prog4/Assets/Scripts/MoveAndRotateTowards.cs
@@ -15,22 +15,14 @@ namespace NodeCanvas.Tasks.Actions {
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
-            agentBlackboard = agent.GetComponent<Blackboard>();
-
-			if (agentBlackboard != null)
-			{
-                return null;
-            }
-			return $"Unable to get {agent.name} blackboard refferance";
+           return null;
         }
 
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			//moveSpeed = agentBlackboard.GetVariableValue<float>("moveSpeed");
-            //turnSpeed = agentBlackboard.GetVariableValue<float>("rotateSpeed");
-			//stoppingDistance = agentBlackboard.GetVariableValue<float>("stopingDistance");
+			
         }
 
 		//Called once per frame while the action is active.
