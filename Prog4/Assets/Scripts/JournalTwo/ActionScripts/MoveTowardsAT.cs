3ca0cd69dbe49498061afb9b1a19ffc6df62f3f3
valor87 <134859690+valor87@users.noreply.github.com>
2026-01-19 17:38:45 -0500

    Starting the interaction

--- Prog4/Assets/Scripts/JournalTwo/ActionScripts/MoveTowardsAT.cs
+++ Prog4/Assets/Scripts/JournalTwo/ActionScripts/MoveTowardsAT.cs
@@ -6,7 +6,9 @@ namespace NodeCanvas.Tasks.Actions {
 	public class MoveTowardsAT : ActionTask {
 
         public Transform closet;
-
+		public BBParameter<float> moveSpeed;
+        public BBParameter<float> rotationSpeed;
+        public BBParameter<float> stoppingDistance;
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -17,13 +19,19 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
+			Vector3 Pos = agent.transform.position;
+			Vector3 Destination = Pos - closet.position;
+			Quaternion Rotation = Quaternion.LookRotation(Destination);
+			agent.transform.SetPositionAndRotation(agent.transform.position + moveSpeed.value * Time.deltaTime * agent.transform.forward, Quaternion.RotateTowards(agent.transform.rotation, Rotation, rotationSpeed.value * Time.deltaTime));
+			if (Vector3.Distance(Pos, closet.position) < stoppingDistance.value)
+			{
+                EndAction(true);
+            }
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
