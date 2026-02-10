26373b53eec2c0e85a8901cb83e240cb1e42b0f4
valor87 <134859690+valor87@users.noreply.github.com>
2026-02-10 01:51:12 -0500

    changed the way that the player sets the full trash

--- Prog4/Assets/Scripts/Assignment4/PlayerThrowingOutTrash.cs
+++ Prog4/Assets/Scripts/Assignment4/PlayerThrowingOutTrash.cs
@@ -7,23 +7,22 @@ namespace NodeCanvas.Tasks.Actions {
     {
         public Blackboard CoyoteBBP;
         public BBParameter<bool> GarbageFull;
-        
+        protected override void OnExecute()
+        {
+            CoyoteBBP.SetVariableValue("GarbageFull", false);
+            base.OnExecute();
+        }
         protected override void OnUpdate()
         {
             if (Input.GetMouseButtonDown(0))
             {
-                GarbageFull.value = true;
+                CoyoteBBP.SetVariableValue("GarbageFull", true);
+                EndAction();
 
             }
-            CoyoteBBP.SetVariableValue("GarbageFull", GarbageFull.value);
-            if (GarbageFull.value)
-                EndAction();
+
         }
 
-        protected override void OnStop()
-        {
-            CoyoteBBP.SetVariableValue("GarbageFull", GarbageFull.value);
-        }
     }
     
 }
